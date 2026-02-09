50370f02b21ab4d26ab9a4fa44c0d84795b88abb
Ramprasad <ramprasad6352@gmail.com>
2026-02-09 18:10:26 +0530

    Updated gitignore and backend updates

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://16.170.230.170:8080',
+  apiBaseUrl1: 'http://16.170.230.170:8080'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080', // Update this with your API URL
-  apiUrl1: 'http://localhost:8080/user'
+  // apiUrl: 'http://localhost:8080', // Update this with your API URL
+  apiUrl: 'http://16.170.230.170:8080', // Update this with your API URL
+  apiUrl1: 'http://16.170.230.170:8080/user'
 };
